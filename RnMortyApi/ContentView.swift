a32574abc008ce6644c832f269be5db55356886a
Arild Frøslie <arildfroslie47@gmail.com>
2023-10-09 16:25:10 +0200

    ?

--- RnMortyApi/ContentView.swift
+++ RnMortyApi/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView{
+            HomeView()
+                .tabItem{
+                    Label("Home", systemImage: "house.fill")
+                }
+            CharacterListView()
+                .tabItem{
+                    Label("Characters", systemImage: "figure.stand")
+                }
+            LocationListView()
+                .tabItem{
+                    Label("Locations", systemImage: "location.circle.fill")
+                }
+            EpisodeView()
+                .tabItem{
+                    Label("Episodes", systemImage: "play.square.stack")
+                }
         }
-        .padding()
     }
 }
 
