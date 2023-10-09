3a4726c87b8c3cb0b939121a112a8dd92691afbd
Arild Frøslie <arildfroslie47@gmail.com>
2023-10-09 12:31:00 +0200

    Initial Commit

---
+++ RnMortyApi/RnMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RnMortyApiApp.swift
+//  RnMortyApi
+//
+//  Created by Arild Frøslie on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RnMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
